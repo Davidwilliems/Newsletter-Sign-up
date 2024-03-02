b5410ef7bdb11a1b9ad2cae9f902f596e11bfedf
Davidwilliems <williamsdavid833@gmail.com>
2024-03-02 15:55:01 +0100

    Use EJS instead of HTML

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
+var email = "";
+var emailCap = {};
 
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.use(express.static("public"));
+
+function emailReg(req, res, next){
+    email = req.body.email;
+    emailCap = {
+        email : email
+    };
+    next();
+}
+
+app.use(emailReg);
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    res.render("index.ejs", emailCap);
+});
 
 app.listen(port, () => {
     console.log("Listening on Port: " + port);
